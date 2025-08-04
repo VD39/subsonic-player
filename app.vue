a001d59a7fd48408a06b4268a01ed1181f214ae1
Vijay Dubb <13175331+VD39@users.noreply.github.com>
2025-08-04 20:05:41 +0200

    Temporary disable enableDragDropTouch.

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { enableDragDropTouch } from '@dragdroptouch/drag-drop-touch';
+// import { enableDragDropTouch } from '@dragdroptouch/drag-drop-touch';
 
 import MainLoader from '@/components/Atoms/MainLoader.vue';
 import ModalWindow from '@/components/Molecules/ModalWindow.vue';
@@ -31,12 +31,12 @@ useHead({
   },
 });
 
-onMounted(() => {
-  // Enable for touch related devices.
-  enableDragDropTouch(undefined, undefined, {
-    isPressHoldMode: true,
-  });
-});
+// onMounted(() => {
+//   // Enable for touch related devices.
+//   enableDragDropTouch(undefined, undefined, {
+//     isPressHoldMode: true,
+//   });
+// });
 </script>
 
 <template>
