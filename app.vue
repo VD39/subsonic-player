800308f3814115373aefaa326898bdfcad57b9a7
Vijay Dubb <13175331+VD39@users.noreply.github.com>
2025-10-14 17:48:07 +0200

    Remove drag and drop functionality for touch devices.

--- app.vue
+++ app.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-// import { enableDragDropTouch } from '@dragdroptouch/drag-drop-touch';
-
 import MainLoader from '@/components/Atoms/MainLoader.vue';
 import ModalWindow from '@/components/Molecules/ModalWindow.vue';
 import SnackBar from '@/components/Molecules/SnackBar.vue';
@@ -30,13 +28,6 @@ useHead({
     },
   },
 });
-
-// onMounted(() => {
-//   // Enable for touch related devices.
-//   enableDragDropTouch(undefined, undefined, {
-//     isPressHoldMode: true,
-//   });
-// });
 </script>
 
 <template>
